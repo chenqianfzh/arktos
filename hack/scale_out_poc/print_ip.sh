776464f76db6e2af1c64b7460ae44078d8a7ec92
Ubuntu <ubuntu@ip-172-31-26-146.us-east-2.compute.internal>
2021-02-02 22:19:26 +0000

    print ip

---
+++ hack/scale_out_poc/print_ip.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+echo "To see live Prometheus: "
+export GCE_PROJECT=${GCE_PROJECT:-"workload-controller-manager"}
+export GCE_REGION=${GCE_REGION:-"us-central1-b"}
+export RUN_PREFIX=${RUN_PREFIX:-"qian-verify"}
+export SCALEOUT_TP_COUNT="${SCALEOUT_TP_COUNT:-1}"
+
+function get_ip_addr {
+    instance_name=$1
+    IP_ADDR=$(gcloud compute instances describe ${instance_name} --zone "${GCE_REGION}" --project "${GCE_PROJECT}"  --format='get(networkInterfaces[0].accessConfigs[0].natIP)')
+    printf " ${instance_name} : ${IP_ADDR}\n"
+}
+
+get get_ip_addr ${RUN_PREFIX}-kubemark-rp-master
+
+for (( tp_num=1; tp_num<=${SCALEOUT_TP_COUNT}; tp_num++ ))
+do
+  get_ip_addr ${RUN_PREFIX}-kubemark-tp-${tp_num}-master
+done
+
+get_ip_addr ${RUN_PREFIX}-kubemark-rp-master
+
+get_ip_addr ${RUN_PREFIX}-master
+
+get_ip_addr ${RUN_PREFIX}-kubemark-proxy
+
+for minion in $(gcloud compute instance-groups list-instances ${RUN_PREFIX}-minion-group --zone "${GCE_REGION}" --project "${GCE_PROJECT}")
+do 
+  echo "------------ $minion"
+  get_ip_addr $minion
+done
